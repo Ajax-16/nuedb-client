3cbb93ee0fce8ec467d10af53d2f154036fd0961
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-02-17 20:56:06 +0100

    added data chunk support

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ export default function connect(hostname, port, commands) {
   return new Promise((resolve, reject) => {
     const PORT = port;
     const HOST = hostname;
-    let commandsQueue = [];
-    let finalResult = [];
+    let responses = []; // Array para almacenar todas las respuestas
 
     const client = new net.Socket();
 
     client.connect(PORT, HOST, async () => {
-
       await processCommands();
-
       client.end();
     });
 
     client.on('end', () => {
-      resolve(finalResult);
+      // Se ha completado la transmisión de datos, resolver la promesa con todas las respuestas
+      resolve(responses);
     });
 
     client.on('error', (err) => {
@@ -26,34 +24,46 @@ export default function connect(hostname, port, commands) {
     });
 
     async function processCommands() {
-      commandsQueue = commands.split(';');
+      const commandsQueue = commands.split(';').map(command => command.trim()).filter(command => command); // Filtrar comandos vacíos
 
       for (const command of commandsQueue) {
-        const trimmedCommand = command.trim();
-        if (trimmedCommand) {
-          try {
-            const result = await sendCommand(trimmedCommand);
-            finalResult.push(result);
-          } catch (error) {
-            console.error('Error processing command:', error.message);
-            finalResult = '';
-          }
+        try {
+          const result = await sendCommand(command);
+          // Agregar la respuesta al array de respuestas
+          responses.push(result);
+        } catch (error) {
+          console.error('Error processing command:', error.message);
+          // Agregar una respuesta vacía al array en caso de error
+          responses.push('');
         }
       }
     }
 
     function sendCommand(command) {
       return new Promise((resolve, reject) => {
+        let partialResult = ''; // Variable para almacenar el fragmento de la respuesta
+    
         client.write(command);
-        client.once('data', (data) => {
-          const result = JSON.parse(data);
-          if (result.error) {
-            reject(new Error(result.error));
+        client.on('data', (data) => {
+          const result = data.toString();
+          
+          // Comprueba si se ha recibido la marca de fin de respuesta
+          if (result.includes('END_OF_RESPONSE')) {
+            // Concatena el todos los chunks de respuesta sin la marca de fin de respuesta
+            partialResult += result.replace('END_OF_RESPONSE', '');
+
+            const parsedResult = JSON.parse(partialResult);
+            // Resuelve la promesa con la respuesta completa parseada a JSON
+            resolve(parsedResult);
+            // Limpia partialResult para la próxima respuesta ya que al mandarse varios paquetes, este evento se va a reproducir varias veces
+            partialResult = '';
           } else {
-            resolve(result);
+            // Concatena el chunk de respuesta al parcialResult
+            partialResult += result;
           }
         });
       });
     }
+        
   });
 }
