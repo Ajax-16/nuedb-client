0267af25608c182bc7ae2567f56e44dbea4efff4
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-04-26 17:29:42 +0200

    core clien v0.2.0

--- components/display.js
+++ components/display.js
@@ -1,49 +1,54 @@
 import chalk from "chalk";
 import CliTable3 from "cli-table3";
 
-export function display(result) {
+export function display(responses) {
 
-    for (const element of result) {
-        const {body, headers} = element;
-        const {Status} = headers;
-        if (typeof body === 'string' && Status === "OK") {
-            console.log(chalk.green.bold(body + "\n")); 
-        }else if(typeof body === 'string' && Status === "ERROR"){
-            console.log(chalk.red.bold(body + "\n")); 
-        }else if(typeof body === 'boolean') {
+    for (const response of responses) {
+        
+        for (const element of response.body) {
+            
+            const {headers} = response;
+            const {Status} = headers;
 
-            body ? console.log(chalk.green.bold('command execution succeed!\n')) : console.log(chalk.yellow.bold('command execution didn\'t make any changes!\n'));
-
-        }else if (Array.isArray(body)) {
-
-            const cliTable = new CliTable3();
-
-            if(!isBidemensional(body)){
-
-                const titleRow = body.map(item => chalk.green.bold(item.toString().toUpperCase()));
-                cliTable.push(titleRow);
-
-            }else{
-
-                const tableName = body[0]; // Obtener el nombre de la tabla
-                const headers = body[1]; // Obtener los encabezados
+            if (typeof element === 'string' && Status === "OK") {
+                console.log(chalk.green.bold(element + "\n")); 
+            }else if(typeof element === 'string' && Status === "ERROR"){
+                console.log(chalk.red.bold(element + "\n")); 
+            }else if(typeof element === 'boolean') {
     
-                cliTable.push([{ colSpan: headers.length, content: tableName[0] !== 'EXCEPTION ENCOUNTER' ? chalk.green.bold(tableName.toString().toUpperCase()) : chalk.red.bold(tableName.toString().toUpperCase()) }]); // Añadir nombre de la tabla con colSpan
-                
-                cliTable.push(headers.map(header => chalk.bold(header)));
+                element ? console.log(chalk.green.bold('command execution succeed!\n')) : console.log(chalk.yellow.bold('command execution didn\'t make any changes!\n'));
     
-                for (let i = 2; i < body.length; i++) {
-                    body[i] = body[i].map(value => value === null ? chalk.yellow('null') : value);
-                    cliTable.push(body[i]);
+            }else if (Array.isArray(element)) {
+    
+                const cliTable = new CliTable3();
+    
+                if(!isBidemensional(element)){
+    
+                    const titleRow = element.map(item => chalk.green.bold(item.toString().toUpperCase()));
+                    cliTable.push(titleRow);
+    
+                }else{
+    
+                    const tableName = element[0]; // Obtener el nombre de la tabla
+                    const headers = element[1]; // Obtener los encabezados
+        
+                    cliTable.push([{ colSpan: headers.length, content: tableName[0] !== 'EXCEPTION ENCOUNTER' ? chalk.green.bold(tableName.toString().toUpperCase()) : chalk.red.bold(tableName.toString().toUpperCase()) }]); // Añadir nombre de la tabla con colSpan
+                    
+                    cliTable.push(headers.map(header => chalk.bold(header)));
+        
+                    for (let i = 2; i < element.length; i++) {
+                        element[i] = element[i].map(value => value === null ? chalk.yellow('null') : value);
+                        cliTable.push(element[i]);
+                    }
+    
                 }
-
+    
+                console.log(cliTable.toString());
+                console.log(''); // salto de línea
+    
             }
-
-            console.log(cliTable.toString());
-            console.log(''); // salto de línea
-
+    
         }
-
     }
 
 }
