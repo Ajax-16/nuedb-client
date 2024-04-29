17b2577ec35f922e6e50773f66ca353a67096c66
Ajax-16 <davidbernardezluque7618@gmail.com>
2024-04-29 08:54:48 +0200

    added byte limit to each command chunk

--- io/reqChunk.js
+++ io/reqChunk.js
@@ -1,22 +1,39 @@
 import { config } from "dotenv";
 
 config({ path: '../.env' });
-const CHUNK_SIZE = process.env.CHUNK_SIZE || 4096; // Asegúrate de convertir el valor a entero
+const CHUNK_SIZE = process.env.CHUNK_SIZE || 4096;
+const BYTE_CHUNK_SIZE = process.env.BYTE_CHUNK_SIZE || 32768;
 
 export const createCommandsChunks = (data) => {
-
     const splitCommands = data.split(';').map(command => command.trim()).filter(command => command !== '');
     let commandChunks = [];
+    let currentChunkSize = 0;
+    let currentCommandsCount = 0;
+    let currentChunkCommands = [];
 
-    if (splitCommands.length <= CHUNK_SIZE) {
-        commandChunks.push(splitCommands.join(';'));
-        
-    } else {
-        for (let i = 0; i < splitCommands.length; i += CHUNK_SIZE) {
-            const chunk = splitCommands.slice(i, i + CHUNK_SIZE).join(';'); // Unir los chunks antes de agregarlos
-            commandChunks.push(chunk);
+    for (let i = 0; i < splitCommands.length; i++) {
+        const command = splitCommands[i];
+        const commandSize = Buffer.byteLength(command, 'utf8');
+
+        // Verificar si agregar este comando excederá el tamaño de bytes por chunk
+        if (currentChunkSize + commandSize <= BYTE_CHUNK_SIZE && currentCommandsCount < CHUNK_SIZE) {
+            currentChunkSize += commandSize;
+            currentCommandsCount++;
+            currentChunkCommands.push(command);
+        } else {
+            // Si agregar este comando excederá el límite, o ya tenemos suficientes comandos en el chunk,
+            // agregamos el chunk actual a la lista y empezamos uno nuevo
+            commandChunks.push(currentChunkCommands.join(';'));
+            currentChunkCommands = [command];
+            currentChunkSize = commandSize;
+            currentCommandsCount = 1;
         }
     }
 
-    return commandChunks
+    // Agregar el último chunk si hay comandos restantes
+    if (currentChunkCommands.length > 0) {
+        commandChunks.push(currentChunkCommands.join(';'));
+    }
+
+    return commandChunks;
 }
